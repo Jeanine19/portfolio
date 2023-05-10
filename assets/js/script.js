06c23e147875aeeda9a05a2d97cf2c67530ea479
Jeanine <jeanine.a.santiago@gmail.com>
2023-05-10 12:17:35 -0300

    update

---
+++ assets/js/script.js
@@ -0,0 +1,75 @@
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+
+let nomeOk = false;
+let emailOk = false;
+
+function validarNome() {
+    let txtNome = document.querySelector("#txtNome");
+  
+    if (nome.value.length < 3) {
+      txtNome.innerHTML = "Nome muito curto";
+      txtNome.style.color = "red";
+      nomeOk = false;
+    } else {
+      txtNome.innerHTML = "✔";
+      txtNome.style.color = "green";
+      nomeOk = true;
+    }
+  }
+
+  function validarEmail() {
+    let txtEmail = document.querySelector("#txtEmail");
+  
+    if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+      txtEmail.innerHTML = "E-mail inválido";
+      txtEmail.style.color = "red";
+      emailOk = false;
+    } else {
+      txtEmail.innerHTML = "✔";
+      txtEmail.style.color = "green";
+      emailOk = true;
+    }
+  }
+
+function validarEmailregEx() {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let txtEmail = document.querySelector("#txtEmail");
+  
+    if (email.value.match(regex)) {
+      txtEmail.innerHTML = "✔";
+      txtEmail.style.color = "green";
+      emailOk = true;
+    } else {
+      txtEmail.innerHTML = "E-mail inválido";
+      txtEmail.style.color = "red";
+      emailOk = false;
+    }
+  }
+  function enviarForm() {
+    if (nomeOk === true && emailOk === true && mensagemOk === true) {
+      alert(nome.value + ", obrigado pelo contato, aguarde nosso retorno.");
+    } else {
+      alert("Por favor, preencha todos os campos corretamente.");
+    }
+  }
+  function consultarCep() {
+
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => {
+      if (jsonBody.erro === true || jsonBody.logradouro === undefined) {
+        alert("CEP nao encontrado!");
+      } else {
+        document.getElementById("dados").innerHTML =
+          jsonBody.logradouro +
+          "\n" +
+          jsonBody.bairro +
+          "\n" +
+          jsonBody.localidade +
+          "\n" +
+          jsonBody.uf;
+      }
+    })
+}
---
+++ teste.js
@@ -0,0 +1,11 @@
+console.log("hello World !")
+
+let nome = "Jeanine"
+
+const sobrenome = "Santiago"
+
+console.log(nome + ""+ sobrenome)
+
+let numero1 = 30 
+let numero2 = 10 
+console.log(num1 + num2)
